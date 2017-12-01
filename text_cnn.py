1d44530510b1c135f16c5a02a9dd751b4551244d
root <root@DESKTOP-E1D1855.localdomain>
2017-12-01 16:22:41 +0800

    34类update

--- text_cnn.py
+++ text_cnn.py
@@ -56,3 +56,4 @@ class TextCNN(object):
         with tf.name_scope('accuracy'):
             correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
             self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+
